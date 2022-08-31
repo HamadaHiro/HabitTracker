88188bb8633c208e00ea83603e53bd146b461453
Serigne Moustapha Sy Ndoye <s.moustapha.sy.ndoye@gmail.com>
2022-08-31 23:56:59 +0000

    Initial Set Of Files

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Hiro Hamada on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
